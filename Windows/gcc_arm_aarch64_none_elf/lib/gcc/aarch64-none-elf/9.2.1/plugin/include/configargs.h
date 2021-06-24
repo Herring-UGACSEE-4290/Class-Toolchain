8636f6d69b22a6141d4ac0e074631707fd4cf5a8
Kevin Koffroth <ktkoffroth@gmail.com>
2021-06-23 21:40:54 -0400

    Added Files

---
+++ Windows/gcc_arm_aarch64_none_elf/lib/gcc/aarch64-none-elf/9.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/lto_build/src/gcc/configure --target=aarch64-none-elf --prefix=/tmp/lto_build/build-newlib-aarch64-none-elf/install// --with-gmp=/tmp/lto_build/build-newlib-aarch64-none-elf/host-tools --with-mpfr=/tmp/lto_build/build-newlib-aarch64-none-elf/host-tools --with-mpc=/tmp/lto_build/build-newlib-aarch64-none-elf/host-tools --with-isl=/tmp/lto_build/build-newlib-aarch64-none-elf/host-tools --disable-shared --disable-nls --disable-threads --disable-tls --enable-checking=release --enable-languages=c,c++,fortran --with-newlib --with-pkgversion='GNU Toolchain for the A-profile Architecture 9.2-2020.02 (arm-9.10)' --with-bugurl=https://bugs.linaro.org/";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
